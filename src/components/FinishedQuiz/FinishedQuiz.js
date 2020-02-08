3eaee9037c8d0fcc3f91d594ca51903d5657d9b6
Rainspistols <ffly@mail.ru>
2020-02-08 19:38:45 +0100

    add link for button (go to the test list)

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './FinishedQuiz.module.css';
 import Button from '../UI/Button/Button';
+import { Link } from 'react-router-dom';
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -39,9 +40,9 @@ const FinishedQuiz = props => {
         <Button onClick={props.onRetry} type='primary'>
           Repeat
         </Button>
-        <Button type='success'>
-          Go to the test list
-        </Button>
+        <Link to={'/'}>
+          <Button type='success'>Go to the test list</Button>
+        </Link>
       </div>
     </div>
   );
