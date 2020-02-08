9d49d81aaba673768afd23fbbc41c0943cd99b53
Rainspistols <ffly@mail.ru>
2020-02-08 21:56:19 +0100

    form validation part 1

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,12 +4,63 @@ import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 
 class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Please enter a valid email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Password',
+        errorMessage: 'Please enter a valid password',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  };
+
   loginHandler = () => {};
 
   registerHandler = () => {};
   submitHandler = e => {
     e.preventDefault();
   };
+  onChangeHandler = (e, controlName) => {
+    console.log(`${controlName}: `, e.target.value);
+  };
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={e => this.onChangeHandler(e, controlName)}
+        />
+      );
+    });
+  }
 
   render() {
     return (
@@ -18,8 +69,7 @@ class Auth extends Component {
           <h1>Authorization</h1>
 
           <form className={classes.AuthForm} onSubmit={this.submitHandler}>
-            <Input label='Email'/>
-            <Input label='Password'/>
+            {this.renderInputs()}
 
             <Button type='success' onClick={this.loginHandler}>
               Log in
