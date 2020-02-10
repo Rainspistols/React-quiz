f7f9802545010c81fdf2c29271eb544a2c8093e6
Rainspistols <ffly@mail.ru>
2020-02-10 19:54:43 +0100

    add firebase authorization with apikey though the mail

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import is from 'is_js';
+import axios from 'axios';
 
 class Auth extends Component {
   state = {
@@ -35,9 +36,41 @@ class Auth extends Component {
     }
   };
 
-  loginHandler = () => {};
+  loginHandler = async () => {
+    const authDate = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToker: true
+    };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDgvt3DjP3kUGWrTCsy7JZdzjtqMx-Xxik',
+        authDate
+      );
 
-  registerHandler = () => {};
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  registerHandler = async () => {
+    const authDate = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToker: true
+    };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDgvt3DjP3kUGWrTCsy7JZdzjtqMx-Xxik',
+        authDate
+      );
+
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   submitHandler = e => {
     e.preventDefault();
